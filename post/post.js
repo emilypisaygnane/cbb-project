2013b8c1236d53cf05f76ca48a00c4bc8866c1bf
Emily <episaygnane@me.com>
2022-08-17 11:44:48 -0700

    in post js import checkauth getpost deletepost and renderpost, added delete button

--- post/post.js
+++ post/post.js
@@ -1,2 +1,30 @@
-import { checkAuth, getPost, deletePost } from "../fetch-utils";
-import { renderPosts } from "../render-utils";
+import { checkAuth, getPost, deletePost } from "../fetch-utils.js";
+import { renderPost } from "../render-utils.js";
+
+const postContainerEl = document.querySelector('#post-container');
+const params = new URLSearchParams(window.location.search);
+
+loadData();
+
+const user = checkAuth();
+
+async function loadData() {
+    const postId = params.get('id');
+
+    const details = await getPost(postId);
+
+    const detailsDiv = renderPost(details);
+    postContainerEl.append(detailsDiv);
+
+    if (user.id === details.user.id) {
+        const deleteButtonEl = document.createElement('button');
+        deleteButtonEl.textContent = 'Delete Post';
+        deleteButtonEl.addEventListener('click', async () => {
+            await deletePost(postId);
+            alert('Your Post Has Been Deleted!');
+            location.href = '../';
+
+        });
+        postContainerEl.append(deleteButtonEl);
+    }
+}
