31e6d3017bd5651f32136d8cadddf990a651bfbc
Emily <episaygnane@me.com>
2022-08-18 18:06:22 -0700

    added eventlistener for form and function for display profs into profile js, and added supa key and url to fetch

---
+++ profile/profile.js
@@ -0,0 +1,34 @@
+import { checkAuth, saveProfile, getProfile } from '../fetch-utils.js';
+
+const formEl = document.getElementById('user-form');
+const userNameInput = document.getElementById('user-name');
+const userBioInput = document.getElementById('user-bio');
+
+const userProfile = {
+    name: '';
+    bio: '';
+
+};
+
+const user = checkAuth();
+
+formEl.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const data = new FormData(formEl);
+
+    userProfile.name = data.get('user-name');
+    userProfile.bio = data.get('user-bio');
+
+    await saveProfile(userProfile);
+});
+
+async function displayProfile() {
+    const response = await getProfile(user.id);
+
+    if (response) {
+        userNameInput.value = response.name;
+        userBioInput.value = response.bio;
+    }
+}
+displayProfile();
